572709c467be95f66ef1e495df73f00f4235a2cb
Muhammad Maarij <l192347@lhr.nu.edu.pk>
2023-05-14 19:11:41 +0500

    added graph view option using highcharts.js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,6 +1,9 @@
+		var chart; // global variuable for chart
+		var dataTopics = new Array();
+		
 		// Create a client instance
 		var client = new Paho.MQTT.Client("91.121.93.94", Number(8080), "clientId");
-
+		
 		// set callback handlers for the MQTT client
 		client.onConnectionLost = onConnectionLost;
 		client.onMessageArrived = onMessageArrived;
@@ -16,8 +19,19 @@
 			console.log("Connected to MQTT broker");
 			// subscribe to the temperature, humidity, and light topics
 			client.subscribe("maarij/iotProject/temp");
+			chart.addSeries({id: 0, name: "Temperature", data: [], color: "red"}); //add the series
+
 			client.subscribe("maarij/iotProject/humidity");
+			chart.addSeries({id: 1, name: "Humidity", data: [], color: "green"});
+
 			client.subscribe("maarij/iotProject/light");
+			chart.addSeries({id: 2, name: "Light", data: [], color: "orange"});
+		
+			Highcharts.setOptions({
+				global: {
+					useUTC: false
+				}
+			});
 		}
 
 		// called when the client fails to connect
@@ -44,6 +58,10 @@
 				} else {
 				  document.getElementById("tempLED").style.backgroundColor = "#bbb";
 				}
+
+				var myEpoch = new Date().getTime(); //get current epoch time
+				var plotMqtt = [myEpoch, parseFloat(message.payloadString)]; //create the array
+				plot(plotMqtt, 0);
 	
 			} else if (message.destinationName === "maarij/iotProject/humidity") {
 				// display the humidity value in the web page
@@ -54,6 +72,10 @@
 				} else {
 				  document.getElementById("humidityLED").style.backgroundColor = "#bbb";
 				}
+
+				var myEpoch = new Date().getTime(); //get current epoch time
+				var plotMqtt = [myEpoch, parseFloat(message.payloadString)]; //create the array
+				plot(plotMqtt, 1);
 				
 			} else if (message.destinationName === "maarij/iotProject/light") {
 				// display the light value in the web page
@@ -64,5 +86,72 @@
 				} else {
 				  document.getElementById("lightLED").style.backgroundColor = "#bbb";
 				}
+
+				var myEpoch = new Date().getTime(); //get current epoch time
+				var plotMqtt = [myEpoch, parseFloat(message.payloadString)]; //create the array
+				plot(plotMqtt, 2);
+			}
+		}
+
+		//this adds the plots to the chart	
+		function plot(point, chartno) {
+			console.log(point);
+			
+				var series = chart.series[0],
+					shift = series.data.length > 10000; // shift if the series is 
+													 // longer than 10000
+				// add the point
+				chart.series[chartno].addPoint(point, true, shift);  
+	
+		};
+	
+	//settings for the chart
+		$(document).ready(function() {
+			chart = new Highcharts.Chart({
+				chart: {
+					renderTo: 'graphContainer',
+					defaultSeriesType: 'spline',
+					backgroundColor: '#f7f7f7',
+					
+				},
+				title: {
+					text: ''
+				},
+				xAxis: {
+					type: 'datetime',
+					tickPixelInterval: 150,
+					maxZoom: 20 * 1000
+				},
+				yAxis: {
+					minPadding: 0.2,
+					maxPadding: 0.2,
+					title: {
+						text: 'Value',
+						margin: 20
+					}
+				},
+				credits: {
+					enabled: false
+				},
+				series: []
+			});        
+		});
+
+
+		function toggleDiv() {
+			var graphDiv = document.getElementById("graph");
+			var liveDiv = document.getElementById("live");
+			var toggleButton = document.getElementById("toggleBtn");
+	
+			if (graphDiv.style.display == "none") {
+				console.log("graph displayed")
+				graphDiv.style.display = "block";
+				liveDiv.style.display = "none";
+				toggleButton.innerHTML = "Live View"
+			} else {
+				console.log("live displayed")
+				graphDiv.style.display = "none";
+				liveDiv.style.display = "block";
+				toggleButton.innerHTML = "Graph View"
 			}
 		}
